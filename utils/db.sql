041e9a8fbf75ac26348e1646909bb350a44c8623
Akide Liu <mail@llycloud.com>
2020-09-25 06:41:57 +0930

    Fix travis CI database test 01

---
+++ utils/db.sql
@@ -0,0 +1,108 @@
+-- MySQL dump 10.13  Distrib 5.6.47, for osx10.15 (x86_64)
+--
+-- Host: bt-02-test.in.llycloud.com    Database: oop
+-- ------------------------------------------------------
+-- Server version	5.6.49-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `people`
+--
+
+DROP TABLE IF EXISTS `people`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `people` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(255) NOT NULL,
+  `password` char(255) NOT NULL DEFAULT '-1',
+  `title` char(255) NOT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT '1',
+  `userLevel` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `People_user_id_uindex` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `people`
+--
+
+LOCK TABLES `people` WRITE;
+/*!40000 ALTER TABLE `people` DISABLE KEYS */;
+INSERT INTO `people` VALUES (1,'student01','-1','student',1,1),(2,'tutor01','-1','tutor',1,2),(3,'teacher01','-1','teacher',1,3);
+/*!40000 ALTER TABLE `people` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peopleSubject`
+--
+
+DROP TABLE IF EXISTS `peopleSubject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peopleSubject` (
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `subject_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`subject_id`),
+  KEY `peopleSubject_subject_subject_id_fk` (`subject_id`),
+  CONSTRAINT `peopleSubject_People_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `people` (`user_id`),
+  CONSTRAINT `peopleSubject_subject_subject_id_fk` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peopleSubject`
+--
+
+LOCK TABLES `peopleSubject` WRITE;
+/*!40000 ALTER TABLE `peopleSubject` DISABLE KEYS */;
+INSERT INTO `peopleSubject` VALUES (1,1),(2,1),(3,1),(2,2),(3,2);
+/*!40000 ALTER TABLE `peopleSubject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(255) DEFAULT NULL,
+  PRIMARY KEY (`subject_id`),
+  UNIQUE KEY `subject_subject_id_uindex` (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (1,'subject01'),(2,'subject02');
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-09-25  5:52:46
