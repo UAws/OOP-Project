f0b2381b38a4d75475129076941cc4b60caaa579
Akide Liu <mail@llycloud.com>
2020-09-25 23:38:52 +0930

    Change author to authors (whole project)

--- src/service/StudentServices.cpp
+++ src/service/StudentServices.cpp
@@ -1,4 +1,4 @@
-// Author : Akide Liu 
+// Authors: Akide Liu ; Andrew Wang ; Chi Wang
 // Date : 24/9/20
 // License and copyright notice: GNU General Public License v3.0 
 // https://www.gnu.org/licenses/gpl-3.0.en.html
