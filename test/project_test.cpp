0b205cabbedd556d7843538093968aaf0539f24a
Andrew Wang <1281505982@qq.com>
2020-09-26 14:58:35 +0000

    Update login function, replace by database

--- test/project_test.cpp
+++ test/project_test.cpp
@@ -12,40 +12,41 @@
 
 #include <vo/include/VO_PUBLIC.h>
 #include <service/include/SERVICE_PUBLIC.h>
+#include <dao/include/PeopleDao.h>
 
 using namespace std;
 
-TEST(major,test01){
+// TEST(major,test01){
 
-    //TODO: storage need to change to dao by database
-    for (const auto& kv : Storage::storagePeople) {
-        std::cout << kv.first << " has value " << *kv.second <<" userLevel : "<<kv.second->getUserLevel()<<std::endl;
-    }
-    //TODO: need to replace by database
-    for (const auto& kv : Storage::storageSubjects) {
-        std::cout << kv.first << " has value " << kv.second <<std::endl;
-    }
+//     //TODO: storage need to change to dao by database
+//     for (const auto& kv : Storage::storagePeople) {
+//         std::cout << kv.first << " has value " << *kv.second <<" userLevel : "<<kv.second->getUserLevel()<<std::endl;
+//     }
+//     //TODO: need to replace by database
+//     for (const auto& kv : Storage::storageSubjects) {
+//         std::cout << kv.first << " has value " << kv.second <<std::endl;
+//     }
 
-    //TODO: need to replace by database
-    cout << Storage::title_array << std::endl;
+//     //TODO: need to replace by database
+//     cout << Storage::title_array << std::endl;
 
-    // Storage::setCurrentUserById(2);
-    // Storage::storagePeople[2]->login(4, "-1");
-    // Storage::storagePeople[2]->showStudents();
-    // Storage::storagePeople[2]->addNewStudent(new Student(Storage::getSUserId(), "student02"));
-    // Storage::storagePeople[2]->showStudents();
-    //
-    PeopleServices::ListAllUsers();
+//     // Storage::setCurrentUserById(2);
+//     // Storage::storagePeople[2]->login(4, "-1");
+//     // Storage::storagePeople[2]->showStudents();
+//     // Storage::storagePeople[2]->addNewStudent(new Student(Storage::getSUserId(), "student02"));
+//     // Storage::storagePeople[2]->showStudents();
+//     //
+//     PeopleServices::ListAllUsers();
 
-    PeopleServices *ps01 = new TutorServices();
-    ps01->showStudents();
+//     PeopleServices *ps01 = new TutorServices();
+//     ps01->showStudents();
 
 
 
-    //TODO: need to replace by database
-    Storage::clearHeap();
+//     //TODO: need to replace by database
+//     Storage::clearHeap();
 
-}
+// }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
@@ -54,4 +55,23 @@ int main(int argc, char **argv) {
     // ::testing::GTEST_FLAG(filter) = "major*";
 
     return RUN_ALL_TESTS();
+}
+
+
+
+
+TEST(major,login) {
+
+    for(int i = 1; i < 10; i++){
+        bool result =  PeopleServices::login(i,"-1");
+
+    // Student *check1 = new Student(1,"-1");
+        if(i < 6){
+        // bool result =  PeopleServices::login(1,"-1");
+        EXPECT_EQ(result, true);   
+        } else {
+        EXPECT_EQ(result, false);   
+    }  
+    }
+
 }
