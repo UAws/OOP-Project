190cf7c193412c7c65589178d0854ed746d82184
Akide Liu <mail@llycloud.com>
2020-10-06 11:30:10 +1030

    Update license notice

--- utils/database_connection.h
+++ utils/database_connection.h
@@ -1,10 +1,27 @@
-// Authors: Akide Liu ; Andrew Wang ; Chi Wang
-// Date : 25/9/20
-// License and copyright notice: GNU General Public License v3.0 
-// https://www.gnu.org/licenses/gpl-3.0.en.html
-// Description : 
+/*
 
-//
+Authors: Akide Liu ; Andrew Wang ; Chi Wang
+Date : 24/9/20
+
+OOP-Project
+Copyright (C) <2020>  Akide Liu ; Andrew Wang ; Chi Wang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Description :
+
+*/
 
 #ifndef OOP_PROJECT_DATABASE_CONNECTION_H
 #define OOP_PROJECT_DATABASE_CONNECTION_H
