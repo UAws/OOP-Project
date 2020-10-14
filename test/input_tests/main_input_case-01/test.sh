7a94f47bd4109e55c97fb4921f085dd10c7a6a31
Akide Liu <mail@llycloud.com>
2020-10-15 09:03:23 +1030

    Fix main test case 1

--- test/input_tests/main_input_case-01/test.sh
+++ test/input_tests/main_input_case-01/test.sh
@@ -2,13 +2,16 @@ CURRENT_DIR=$(pwd)
 
 cd ../../../
 
-mysql oop < utils/db.sql
+#mysql oop < utils/db.sql
 
 EXEFile=$(find "$(pwd -P)"  -name 'MainSelf' | head -1)
 
 cd $CURRENT_DIR
 
-$EXEFile < input.txt > output.txt
+$EXEFile < input.txt \
+  | grep -v '-' \
+  | grep -v 'enter' \
+  > output.txt
 
 if [ $? != "0" ]; then
     exit 1
@@ -17,5 +20,6 @@ fi
 diff output.txt result.txt
 
 if [ $? != "0" ]; then
+    cat output.txt
     exit 1
 fi
