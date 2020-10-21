2c0c25b4f4d773cc6abd09652874bf859e306484
Akide Liu <mail@llycloud.com>
2020-10-21 18:28:35 +1030

    Add main_input_case-02

---
+++ test/input_tests/main_input_case-02/test.sh
@@ -0,0 +1,28 @@
+CURRENT_DIR=$(pwd)
+
+cd ../../../
+
+#mysql oop < utils/db.sql
+
+EXEFile=$(find "$(pwd -P)"  -name 'MainSelf' | head -1)
+
+cd $CURRENT_DIR
+
+$EXEFile < input.txt \
+  | grep -v '-' \
+  | grep -v 'enter' \
+  > output.txt
+
+#if [ $? != "0" ]; then
+#    exit 1
+#fi
+#
+#diff output.txt result.txt
+#
+#if [ $? != "0" ]; then
+#    cat output.txt
+#    exit 1
+#fi
+
+
+# use ofstream to print out test 
